6f3840c81ae0560295e33960c88ddb70e914e367
Natalia Spiridonova <natusic199797@gmail.com>
2017-03-23 20:24:35 +0300

    Childrens app

---
+++ ChildrensApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChildrensApp
+//
+//  Created by Наталья on 23.03.17.
+//  Copyright © 2017 IOSLab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
